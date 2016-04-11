a7c8fd72873b32874c011043ae0e6d5ba84dc088
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-04-11 16:46:30 +0100

    Add array-methods to DetectorInfo

--- cow_instrument/DetectorInfo.h
+++ cow_instrument/DetectorInfo.h
@@ -2,19 +2,56 @@
 #define DETECTOR_INFO_H
 
 #include <memory>
-#include <map>
+#include <vector>
+#include <stdexcept>
+#include <string>
+#include <sstream>
 #include "IdType.h"
 
-template
-<typename InstTree>
-class DetectorInfo {
+template <typename U>
+void rangeCheck(size_t detectorIndex, const std::vector<U> &container) {
+  if (detectorIndex >= container.size()) {
+    std::stringstream buffer;
+    buffer << "Detector at index " << detectorIndex << " is out of range";
+    throw std::out_of_range(buffer.str());
+  }
+}
+
+template <typename InstTree> class DetectorInfo {
 public:
-    DetectorInfo(std::shared_ptr<InstTree>& instrumentTree) : m_instrumentTree(instrumentTree){
-        instrumentTree->fillDetectorMap(m_idToIndexMap);
-    }
+  DetectorInfo(std::shared_ptr<InstTree> &&instrumentTree)
+      : m_isMasked(instrumentTree->nDetectors(), false),
+        m_isMonitor(instrumentTree->nDetectors(), false),
+        m_instrumentTree(instrumentTree) {}
+
+  DetectorInfo(std::shared_ptr<InstTree> &instrumentTree)
+      : m_isMasked(instrumentTree->nDetectors(), false),
+        m_isMonitor(instrumentTree->nDetectors(), false),
+        m_instrumentTree(instrumentTree) {}
+
+  void setMasked(size_t detectorIndex) {
+    rangeCheck(detectorIndex, m_isMasked);
+    m_isMasked[detectorIndex] = true;
+  }
+  bool isMasked(size_t detectorIndex) const {
+    rangeCheck(detectorIndex, m_isMasked);
+    return m_isMasked[detectorIndex];
+  }
+
+  void setMonitor(size_t detectorIndex) {
+    rangeCheck(detectorIndex, m_isMonitor);
+    m_isMonitor[detectorIndex] = true;
+  }
+
+  bool isMonitor(size_t detectorIndex) const {
+    rangeCheck(detectorIndex, m_isMonitor);
+    return m_isMonitor[detectorIndex];
+  }
+
 private:
-    std::shared_ptr<InstTree> m_instrumentTree;
-    std::map<DetectorIdType, size_t> m_idToIndexMap;
+  std::shared_ptr<InstTree> m_instrumentTree;
+  std::vector<bool> m_isMasked;
+  std::vector<bool> m_isMonitor;
 };
 
 #endif
--- cow_instrument/testing/DetectorInfoTest.cpp
+++ cow_instrument/testing/DetectorInfoTest.cpp
@@ -3,31 +3,68 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <map>
+#include <stdexcept>
 #include "IdType.h"
 
 namespace {
 
 class PolymorphicInstrumentTree {
 public:
-  virtual void fillDetectorMap(const std::map<DetectorIdType, size_t> &toFill) const = 0;
+  virtual size_t nDetectors() const = 0;
   virtual ~PolymorphicInstrumentTree() {}
 };
 
 class MockInstrumentTree : public PolymorphicInstrumentTree {
 public:
+  MockInstrumentTree() {}
+  MockInstrumentTree(size_t nDetectors) {
+    ON_CALL(*this, nDetectors()).WillByDefault(testing::Return(nDetectors));
+  }
+  MOCK_CONST_METHOD0(nDetectors, size_t());
   virtual ~MockInstrumentTree() {}
-  MOCK_CONST_METHOD1(fillDetectorMap, void(const std::map<DetectorIdType, size_t> &));
 };
 
 using DetectorInfoWithMockInstrument = DetectorInfo<MockInstrumentTree>;
 
 TEST(detector_info_test, test_construct) {
 
-  MockInstrumentTree* pMockInstrumentTree = new MockInstrumentTree;
+  MockInstrumentTree *pMockInstrumentTree = new MockInstrumentTree;
+  EXPECT_CALL(*pMockInstrumentTree, nDetectors()).WillRepeatedly(testing::Return(10));
+
   std::shared_ptr<MockInstrumentTree> mockInstrumentTree{pMockInstrumentTree};
-  EXPECT_CALL(*pMockInstrumentTree, fillDetectorMap(testing::_)).Times(1);
+
   DetectorInfoWithMockInstrument detectorInfo{mockInstrumentTree};
   EXPECT_TRUE(testing::Mock::VerifyAndClear(pMockInstrumentTree))
       << "InstrumentTree used incorrectly";
+}
+
+TEST(detector_info_test, test_masking) {
+
+  size_t nDetectors = 3;
+
+  DetectorInfoWithMockInstrument detectorInfo{
+      std::make_shared<testing::NiceMock<MockInstrumentTree>>(nDetectors)};
+
+  EXPECT_FALSE(detectorInfo.isMasked(0));
+  detectorInfo.setMasked(0);
+  EXPECT_TRUE(detectorInfo.isMasked(0));
+
+  EXPECT_THROW(detectorInfo.setMasked(nDetectors), std::out_of_range);
+  EXPECT_THROW(detectorInfo.isMasked(nDetectors), std::out_of_range);
+}
+
+TEST(detector_info_test, test_monitors) {
+
+  size_t nDetectors = 3;
+
+  DetectorInfoWithMockInstrument detectorInfo{
+      std::make_shared<testing::NiceMock<MockInstrumentTree>>(nDetectors)};
+
+  EXPECT_FALSE(detectorInfo.isMonitor(0));
+  detectorInfo.setMonitor(0);
+  EXPECT_TRUE(detectorInfo.isMonitor(0));
+
+  EXPECT_THROW(detectorInfo.setMonitor(nDetectors), std::out_of_range);
+  EXPECT_THROW(detectorInfo.isMonitor(nDetectors), std::out_of_range);
 }
 }
