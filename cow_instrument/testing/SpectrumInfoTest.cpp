0485a736711d3c6b738c8b9b473c2413b7d6dcc6
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-07-04 13:35:29 +0100

    Test 1:n spectra to detector mapping
    We do this for L2 functionality.

--- cow_instrument/testing/SpectrumInfoTest.cpp
+++ cow_instrument/testing/SpectrumInfoTest.cpp
@@ -3,37 +3,36 @@
 #include "DetectorInfo.h"
 #include "MockTypes.h"
 
+TEST(spectrum_info_test, test_constructor_lhr) {
+  std::vector<Spectrum> spectra{{1}, {2}, {3}};
+  size_t nDetectors = 3;
+  DetectorInfoWithMockInstrument tree{
+      std::make_shared<testing::NiceMock<MockInstrumentTree>>(nDetectors)};
+  SpectrumInfo<MockInstrumentTree> spectrumInfo(spectra, tree);
 
-TEST(spectrum_info_test, test_constructor_lhr){
-    std::vector<Spectrum> spectra{{1},{2},{3}};
-    size_t nDetectors = 3;
-    DetectorInfoWithMockInstrument tree{
-        std::make_shared<testing::NiceMock<MockInstrumentTree>>(nDetectors)};
-    SpectrumInfo<MockInstrumentTree> spectrumInfo(spectra, tree);
-
-    EXPECT_EQ(3,spectrumInfo.size());
-    EXPECT_EQ(3,spectrumInfo.nDetectors());
+  EXPECT_EQ(3, spectrumInfo.size());
+  EXPECT_EQ(3, spectrumInfo.nDetectors());
 }
 
-TEST(spectrum_info_test, test_spectra_fetch){
+TEST(spectrum_info_test, test_spectra_fetch) {
 
-    std::vector<Spectrum> spectra{{1},{2,3},{4,5,6}};
-    size_t nDetectors = 6;
-    DetectorInfoWithMockInstrument tree{
-        std::make_shared<testing::NiceMock<MockInstrumentTree>>(nDetectors)};
-    SpectrumInfo<MockInstrumentTree> spectrumInfo(spectra, tree);
+  std::vector<Spectrum> spectra{{1}, {2, 3}, {4, 5, 6}};
+  size_t nDetectors = 6;
+  DetectorInfoWithMockInstrument tree{
+      std::make_shared<testing::NiceMock<MockInstrumentTree>>(nDetectors)};
+  SpectrumInfo<MockInstrumentTree> spectrumInfo(spectra, tree);
 
-    EXPECT_EQ(3,spectrumInfo.size());
-    EXPECT_EQ(6,spectrumInfo.nDetectors());
+  EXPECT_EQ(3, spectrumInfo.size());
+  EXPECT_EQ(6, spectrumInfo.nDetectors());
 
-    auto spectrum = spectrumInfo.spectra(0);
-    EXPECT_EQ(spectrum, (Spectrum{1}));
+  auto spectrum = spectrumInfo.spectra(0);
+  EXPECT_EQ(spectrum, (Spectrum{1}));
 
-    spectrum = spectrumInfo.spectra(1);
-    EXPECT_EQ(spectrum, (Spectrum{2,3}));
+  spectrum = spectrumInfo.spectra(1);
+  EXPECT_EQ(spectrum, (Spectrum{2, 3}));
 
-    spectrum = spectrumInfo.spectra(2);
-    EXPECT_EQ(spectrum, (Spectrum{4,5,6}));
+  spectrum = spectrumInfo.spectra(2);
+  EXPECT_EQ(spectrum, (Spectrum{4, 5, 6}));
 }
 
 TEST(spectrum_info_test, test_getl2) {
@@ -71,6 +70,39 @@ TEST(spectrum_info_test, test_getl2) {
 }
 
 TEST(spectrum_info_test, test_getL2_mapped) {
-  EXPECT_TRUE(false) << "TODO. Test similar to above but with multiple "
-                        "detectors in each spectra";
+
+  using namespace testing;
+
+  // Create two detectors
+  size_t nDetectors = 2;
+  MockDetector detectorA, detectorB;
+  // This is where I place one of the the detectors
+  EXPECT_CALL(detectorA, getPos()).WillOnce(testing::Return(V3D{0, 0, 40}));
+  // This is where I place the other of the the detectors
+  EXPECT_CALL(detectorB, getPos()).WillOnce(testing::Return(V3D{0, 0, 30}));
+
+  auto instrument =
+      std::make_shared<testing::NiceMock<MockInstrumentTree>>(nDetectors);
+  EXPECT_CALL(*instrument.get(), getDetector(_))
+      .WillOnce(ReturnRef(detectorA))
+      .WillOnce(ReturnRef(detectorB));
+  EXPECT_CALL(*instrument.get(), samplePos()).WillOnce(Return(V3D{0, 0, 20}));
+
+  // Create a DetectorInfo around the Instrument
+  DetectorInfoWithMockInstrument detectorInfo{instrument};
+
+  // Create a SpectrumInfo around the DetectorInfo
+  // Two detectors in single spectra
+  std::vector<Spectrum> spectra{{0, 1}};
+  SpectrumInfo<MockInstrumentTree> spectrumInfo(spectra, detectorInfo);
+
+  // This is the point of the test. Do we calculate L2 correctly for our dual detector
+  // spectra.
+  EXPECT_EQ(15.0, spectrumInfo.getL2(0)) << "(40 + 30)/2 - 20";
+  EXPECT_TRUE(Mock::VerifyAndClearExpectations(&detectorA))
+      << "Mock Detector used incorrectly";
+  EXPECT_TRUE(Mock::VerifyAndClearExpectations(&detectorB))
+      << "Mock Detector used incorrectly";
+  EXPECT_TRUE(Mock::VerifyAndClearExpectations(&detectorInfo))
+      << "Mock DetectorInfo used incorrectly";
 }
