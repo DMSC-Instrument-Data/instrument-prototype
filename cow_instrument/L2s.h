3f9afae33e49e18361f9b5764fec496f0be7652a
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-07-05 11:38:36 +0100

    l2 flags no longer required

--- cow_instrument/L2s.h
+++ cow_instrument/L2s.h
@@ -1,15 +1,7 @@
 #ifndef L2S_H
 #define L2S_H
 
-#include "Bool.h"
 #include "FixedLengthVector.h"
-
-/**
- * L2 Set Type. Used to determine if the L2 has been set.
- */
-class L2Flags : public FixedLengthVector<L2Flags, Bool> {
-  using FixedLengthVector<L2Flags, Bool>::FixedLengthVector;
-};
 
 /**
  * L2 Values. Used to determine a cached L2.
