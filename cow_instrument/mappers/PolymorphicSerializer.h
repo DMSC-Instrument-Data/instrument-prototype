70944ae86f279fd53283226f3738a4d836a69793
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-09-28 14:42:33 +0100

    export for std::shared_ptr fix

--- cow_instrument/mappers/PolymorphicSerializer.h
+++ cow_instrument/mappers/PolymorphicSerializer.h
@@ -16,13 +16,43 @@
 namespace boost {
 namespace serialization {
 #ifdef CUSTOM_STD_ARRAY_SERIALIZATION
-template <class Archive, class T>
-void serialize(Archive &ar, std::shared_ptr<T> &t,
-               const unsigned int /*file_version*/
-               ) {
-  const T *t_ptr = t.get();
-  ar &boost::serialization::make_nvp("px", t_ptr);
+template<class Archive, class T>
+inline void save(
+    Archive & ar,
+    const std::shared_ptr< T > &t,
+    const unsigned int /* file_version */
+){
+    const T * t_ptr = t.get();
+    ar << boost::serialization::make_nvp("px", t_ptr);
 }
+
+template<class Archive, class T>
+inline void load(
+    Archive & ar,
+    std::shared_ptr< T > &t,
+    const unsigned int /*file_version*/
+){
+
+    T* r;
+    ar >> boost::serialization::make_nvp("px", r);
+    t.reset(r);
+}
+
+template<class Archive, class T>
+inline void serialize(
+    Archive & ar,
+    std::shared_ptr< T > &t,
+    const unsigned int file_version
+){
+    // correct shared_ptr serialization depends upon object tracking
+    // being used.
+    BOOST_STATIC_ASSERT(
+        boost::serialization::tracking_level< T >::value
+        != boost::serialization::track_never
+    );
+    boost::serialization::split_free(ar, t, file_version);
+}
+
 #endif
 }
 }
