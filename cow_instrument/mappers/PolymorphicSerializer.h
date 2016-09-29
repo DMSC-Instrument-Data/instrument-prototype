3693076764cbdbf08ab7d4bd05acdb11c5ed292a
Owen Arnold <owen.arnold@stfc.ac.uk>
2016-09-29 09:07:34 +0100

    Forgot to remove old overload

--- cow_instrument/mappers/PolymorphicSerializer.h
+++ cow_instrument/mappers/PolymorphicSerializer.h
@@ -13,50 +13,6 @@
 #include "cow_ptr.h"
 #include "SharedPtrSerialization.h"
 
-namespace boost {
-namespace serialization {
-#ifdef LEGACY_BOOST_SERIALIZATION
-template<class Archive, class T>
-inline void save(
-    Archive & ar,
-    const std::shared_ptr< T > &t,
-    const unsigned int /* file_version */
-){
-    const T * t_ptr = t.get();
-    ar << boost::serialization::make_nvp("px", t_ptr);
-}
-
-template<class Archive, class T>
-inline void load(
-    Archive & ar,
-    std::shared_ptr< T > &t,
-    const unsigned int /*file_version*/
-){
-
-    T* r;
-    ar >> boost::serialization::make_nvp("px", r);
-    t.reset(r);
-}
-
-template<class Archive, class T>
-inline void serialize(
-    Archive & ar,
-    std::shared_ptr< T > &t,
-    const unsigned int file_version
-){
-    // correct shared_ptr serialization depends upon object tracking
-    // being used.
-    BOOST_STATIC_ASSERT(
-        boost::serialization::tracking_level< T >::value
-        != boost::serialization::track_never
-    );
-    boost::serialization::split_free(ar, t, file_version);
-}
-
-#endif
-}
-}
-
 template <typename MapperFactory> class PolymorphicSerializer {
 private:
   std::vector<std::shared_ptr<typename MapperFactory::MapperFamily>>
