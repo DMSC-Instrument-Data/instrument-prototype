781e27589d3854e89eb0a0f1721c7285b309c812
Owen Arnold <owen.arnold@stfc.ac.uk>
2017-02-28 11:33:00 +0000

    Add some tests

--- cow_instrument/AssemblyInfo.h
+++ cow_instrument/AssemblyInfo.h
@@ -12,7 +12,7 @@ template <typename InstTree> class AssemblyInfo {
 public:
   AssemblyInfo();
   explicit AssemblyInfo(
-      std::shared_ptr<const ComponentInfo<InstTree>> componentInfo);
+      std::shared_ptr<ComponentInfo<InstTree>> componentInfo);
   Eigen::Quaterniond rotation(size_t assemblyIndex) const;
   Eigen::Vector3d position(size_t assemblyIndex) const;
   void moveAssemblyComponent(const size_t assemblyIndex,
@@ -22,14 +22,14 @@ public:
                                const Eigen::Vector3d &center);
 
 private:
-  std::shared_ptr<const ComponentInfo<InstTree>> m_componentInfo;
+  std::shared_ptr<ComponentInfo<InstTree>> m_componentInfo;
   /// Get the component indexes corresponding to branch nodes
   std::shared_ptr<const std::vector<size_t>> m_assemblyComponentIndexes;
 };
 
 template <typename InstTree>
 AssemblyInfo<InstTree>::AssemblyInfo(
-    std::shared_ptr<const ComponentInfo<InstTree>> componentInfo)
+    std::shared_ptr<ComponentInfo<InstTree>> componentInfo)
     : m_componentInfo(componentInfo),
       m_assemblyComponentIndexes(std::make_shared<const std::vector<size_t>>(
           componentInfo->const_instrumentTree().branchNodeComponentIndexes())) {
@@ -37,13 +37,13 @@ AssemblyInfo<InstTree>::AssemblyInfo(
 
 template <typename InstTree>
 Eigen::Vector3d AssemblyInfo<InstTree>::position(size_t assemblyIndex) const {
-  return m_componentInfo->position(m_assemblyComponentIndexes[assemblyIndex]);
+  return m_componentInfo->position((*m_assemblyComponentIndexes)[assemblyIndex]);
 }
 
 template <typename InstTree>
 Eigen::Quaterniond
 AssemblyInfo<InstTree>::rotation(size_t assemblyIndex) const {
-  return m_componentInfo->rotation(m_assemblyComponentIndexes[assemblyIndex]);
+  return m_componentInfo->rotation((*m_assemblyComponentIndexes)[assemblyIndex]);
 }
 
 template <typename InstTree>
@@ -51,7 +51,7 @@ void
 AssemblyInfo<InstTree>::moveAssemblyComponent(const size_t assemblyIndex,
                                               const Eigen::Vector3d &offset) {
 
-  const auto topComponentIndex = m_assemblyComponentIndexes[assemblyIndex];
+  const auto topComponentIndex = (*m_assemblyComponentIndexes)[assemblyIndex];
   const auto componentsToMove =
       m_componentInfo->const_instrumentTree().subTreeIndexes(topComponentIndex);
   for (auto &compIndex : componentsToMove) {
@@ -65,7 +65,7 @@ void AssemblyInfo<InstTree>::rotateAssemblyComponent(
     const size_t assemblyIndex, const Eigen::Vector3d &axis,
     const double &theta, const Eigen::Vector3d &center) {
 
-  const auto topComponentIndex = m_assemblyComponentIndexes[assemblyIndex];
+  const auto topComponentIndex = (*m_assemblyComponentIndexes)[assemblyIndex];
   const auto componentsToRotate =
       m_componentInfo->const_instrumentTree().subTreeIndexes(topComponentIndex);
   for (auto &compIndex : componentsToRotate) {
--- cow_instrument/testing/AssemblyInfoTest.cpp
+++ cow_instrument/testing/AssemblyInfoTest.cpp
@@ -9,8 +9,7 @@
 
 namespace {
 
-std::shared_ptr<const FlatTree> makeInstrumentTree() {
-
+std::shared_ptr<FlatTree> makeInstrumentTree() {
   /*
 
         A
@@ -18,6 +17,8 @@ std::shared_ptr<const FlatTree> makeInstrumentTree() {
  ------------------------------
  |                |           |
  B                C           D
+                              |
+                              E
 
 
   */
@@ -28,10 +29,14 @@ std::shared_ptr<const FlatTree> makeInstrumentTree() {
   a->addComponent(std::unique_ptr<PointSource>(
       new PointSource(Eigen::Vector3d{-1, 0, 0}, ComponentIdType(3))));
 
-  a->addComponent(std::unique_ptr<PointSample>(
+  auto d = std::unique_ptr<CompositeComponent>(
+      new CompositeComponent(ComponentIdType(4)));
+  d->addComponent(std::unique_ptr<PointSample>(
       new PointSample(Eigen::Vector3d{0.1, 0, 0}, ComponentIdType(5))));
 
-  return std::make_shared<const FlatTree>(a);
+  a->addComponent(std::move(d));
+
+  return std::make_shared<FlatTree>(a);
 }
 
 TEST(assembly_info_test, test_construction) {
@@ -39,5 +44,41 @@ TEST(assembly_info_test, test_construction) {
   auto componentInfo =
       std::make_shared<ComponentInfo<FlatTree>>(instrumentTree);
   AssemblyInfo<FlatTree> assemblyInfo{componentInfo};
+}
+
+TEST(assembly_info_test, test_positions) {
+  auto instrumentTree = makeInstrumentTree();
+  auto componentInfo =
+      std::make_shared<ComponentInfo<FlatTree>>(instrumentTree);
+  AssemblyInfo<FlatTree> assemblyInfo{componentInfo};
+
+  EXPECT_TRUE(assemblyInfo.position(0).isApprox(componentInfo->position(0))) << "Assembly 0 is same as Component 0";
+  EXPECT_TRUE(assemblyInfo.position(1).isApprox(componentInfo->position(3))) << "Assembly 1 is same as Component 3";
+}
+
+
+TEST(assembly_info_test, test_rotations) {
+  auto instrumentTree = makeInstrumentTree();
+  auto componentInfo =
+      std::make_shared<ComponentInfo<FlatTree>>(instrumentTree);
+  AssemblyInfo<FlatTree> assemblyInfo{componentInfo};
+
+  EXPECT_TRUE(assemblyInfo.rotation(0).isApprox(componentInfo->rotation(0))) << "Assembly 0 is same as Component 0";
+  EXPECT_TRUE(assemblyInfo.rotation(1).isApprox(componentInfo->rotation(3))) << "Assembly 1 is same as Component 3";
+}
+TEST(assembly_info_test, test_move){
+
+    auto instrumentTree = makeInstrumentTree();
+    auto componentInfo =
+        std::make_shared<ComponentInfo<FlatTree>>(instrumentTree);
+    AssemblyInfo<FlatTree> assemblyInfo{componentInfo};
+
+    auto currentRootPos = assemblyInfo.position(0); // root position
+    auto currentLeafPos = componentInfo->position(1);
+    Eigen::Vector3d delta{1, 0, 0};
+    assemblyInfo.moveAssemblyComponent(0, delta);
+
+    EXPECT_TRUE(assemblyInfo.position(0).isApprox(currentRootPos + delta)) << "Test root moved";
+    EXPECT_TRUE(componentInfo->position(1).isApprox(currentLeafPos + delta)) << "Test leaf moved. Changes should be progated down.";
 }
 }
